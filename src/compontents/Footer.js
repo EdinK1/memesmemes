52afebdbafc4929a771302f5dfcfaa7725f8dd79
EdinK1 <edinkaymakchi@gmail.com>
2019-08-20 12:29:31 -0700

    fix footer issue

--- src/compontents/Footer.js
+++ src/compontents/Footer.js
@@ -5,7 +5,7 @@ export const StyledFooter = styled.footer`
   background: #f4f4f4;
   color: #000;
   padding: 1rem 0;
-  margin-top: 1rem;
+  margin-top: 200px;
 `;
 
 export const Copyright = styled.p`
