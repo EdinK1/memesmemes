44e05863f7bb2aa42a479ef961392281e7ad402b
Edin Kaymakqi <edink@Edins-MacBook-Pro.local>
2019-08-20 02:45:31 -0700

    add form component

---
+++ src/compontents/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+export const StyledFooter = styled.footer`
+  background: #f4f4f4;
+  color: #000;
+  padding: 1rem 0;
+  margin-top: 1rem;
+`;
+
+export const Copyright = styled.p`
+  text-align: center;
+  font-size: 1.1rem;
+`;
+
+export const Footer = () => (
+  <StyledFooter>
+    <Copyright>
+      Created with{" "}
+      <span role='img' aria-label='hidden'>
+        ☕
+      </span>{" "}
+      by Edin Kaymakqi
+    </Copyright>
+  </StyledFooter>
+);
