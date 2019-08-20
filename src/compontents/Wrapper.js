f481a4a49acabc7c56aff4e9e985bdb1dbd6753a
Edin Kaymakqi <edink@Edins-MacBook-Pro.local>
2019-08-20 02:47:31 -0700

    add wrapper component

---
+++ src/compontents/Wrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  background: #fff;
+  max-width: 65%;
+  list-style-type: none;
+  grid-gap: 20px;
+  margin: 0 auto;
+`;
