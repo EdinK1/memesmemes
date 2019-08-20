f888e84b15e088daa34c1a12dc114d81db75b55b
Edin Kaymakqi <edink@Edins-MacBook-Pro.local>
2019-08-20 02:46:40 -0700

    add header component

---
+++ src/compontents/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+import { Title } from "./Title";
+import { Greeting } from "./Greeting";
+
+const StyledHeader = styled.header`
+  min-height: 80vh;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Header = () => (
+  <StyledHeader>
+    <Title>Welcome to MemesMemes</Title>
+    <Greeting>Pick an image and have fun creating your own meme</Greeting>
+  </StyledHeader>
+);
