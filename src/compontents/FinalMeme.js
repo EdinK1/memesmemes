a20166db1b3e12d009ca253dca00876aafa70ba2
Edin Kaymakqi <edink@Edins-MacBook-Pro.local>
2019-08-20 02:45:06 -0700

    add final meme component

---
+++ src/compontents/FinalMeme.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const FinalMeme = styled.img`
+  max-width: 350px;
+  max-height: 500px;
+  height: auto;
+  box-shadow: 3px 2px 10px 0px rgba(0, 0, 0, 0.75);
+  margin-bottom: 1.5rem;
+`;
