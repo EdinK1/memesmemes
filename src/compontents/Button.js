bfca663f5e583177f31391ddfa3b08c17f8bf75e
Edin Kaymakqi <edink@Edins-MacBook-Pro.local>
2019-08-20 02:44:31 -0700

    add back  button component

---
+++ src/compontents/Button.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const BackButton = styled.button`
+  width: 350px;
+  border: none;
+  color: #fff;
+  background: #1c77c6;
+  margin-top: 1rem;
+  padding: 0.85rem 0.45rem;
+  transition: background 0.2s ease-in;
+  font-size: 1.25rem;
+`;
